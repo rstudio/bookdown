bed7b7263410ddaacedcd54826a332f9c38c3878
Yihui Xie <xie@yihui.name>
2015-11-05 00:27:05 -0600

    an output format pdf_book() (basically to post-process the tex output to resolve cross-references)

---
+++ R/latex.R
@@ -0,0 +1,25 @@
+#' @export
+pdf_book = function(..., toc = TRUE, number_sections = TRUE, fig_caption = TRUE) {
+  config = rmarkdown::pdf_document(
+    ..., toc = TRUE, number_sections = number_sections, fig_caption = fig_caption
+  )
+  post = config$post_processor  # in case a post processor have been defined
+  config$post_processor = function(metadata, input, output, clean, verbose) {
+    if (is.function(post)) output = post(metadata, input, output, clean, verbose)
+    f = with_ext(output, '.tex')
+    x = readLines(f, encoding = 'UTF-8', warn = FALSE)
+    x = resolve_refs_latex(x)
+    writeLines(enc2utf8(x), f, useBytes = TRUE)
+    rmarkdown:::latexmk(f, config$pandoc$latex_engine)
+    with_ext(output, '.pdf')
+  }
+  # use labels of the form (\#label) in knitr
+  config$knitr$opts_knit$bookdown.internal.label = TRUE
+  config
+}
+
+resolve_refs_latex = function(x) {
+  x = gsub('@ref\\(((fig|tab):[-[:alnum:]]+)\\)', '\\\\ref{\\1}', x)
+  x = gsub('\\(\\\\#((fig|tab):[-[:alnum:]]+)\\)', '\\\\label{\\1}', x)
+  x
+}
