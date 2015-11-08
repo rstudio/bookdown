9517830e5c39406ff80a5393a6bd0d2aaacba743
Yihui Xie <xie@yihui.name>
2015-11-08 00:09:31 -0600

    closes #1: do not allow duplicate chunk labels

--- R/render.R
+++ R/render.R
@@ -12,9 +12,5 @@
 #'   when merging all \file{.Rmd }files.
 #' @export
 render_book = function(input, ...) {
-  # TODO: do we really want to support duplicate chunk labels?
-  opts = options(knitr.duplicate.label = 'allow')
-  on.exit(options(opts), add = TRUE)
-
   rmarkdown::render(merge_rmd(), ...)
 }
