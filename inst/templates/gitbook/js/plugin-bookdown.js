a829c1f639f5c9a95d543a1d271df41507d6cd3a
Yihui Xie <xie@yihui.name>
2016-02-14 23:33:47 -0600

    add 50px to the top of the book body when the toolbar is fixed

--- inst/templates/gitbook/js/plugin-bookdown.js
+++ inst/templates/gitbook/js/plugin-bookdown.js
@@ -116,6 +116,7 @@ require(["gitbook", "lodash"], function(gitbook, _) {
         // the theme may have changed after user clicks the theme button
         bookHeader.css('background-color', bookBody.css('background-color'));
       });
+      bookBody.css('top', '50px');
     }
 
   });
