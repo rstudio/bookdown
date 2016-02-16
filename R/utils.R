ff867ca7e5a98f91db42daf5490bd8e90d6edfd6
Yihui Xie <xie@yihui.name>
2016-02-16 15:08:35 -0600

    with_ext() does not work with length(x) = 1 & length(ext) > 1

--- R/utils.R
+++ R/utils.R
@@ -19,16 +19,18 @@ next_nearest = function(x, y) {
 # change the filename extension
 with_ext = function(x, ext) {
   n1 = length(x); n2 = length(ext); r = '[.][[:alnum:]]+$'
-  if (n1 == 0) return(x)
+  if (n1 * n2 == 0) return(x)
   i = grep('^[.]', ext, invert = TRUE) & ext != ''
-  ext[i] = paste0('.', ext)
-  y = if (n2 == 1) sub(r, ext, x) else {
-    if (n1 > 1 && n1 != n2) stop("'ext' must be of the same length as 'x'")
-    mapply(sub, '[.][[:alnum:]]+$', ext, x)
-  }
-  i = grep(r, x, invert = TRUE)
-  if (length(i)) y[i] = paste0(x[i], ext)
-  y
+  ext[i] = paste0('.', ext[i])
+
+  if (all(ext == '')) ext = ''
+  if (length(ext) == 1) return(sub(r, ext, x))
+
+  if (n1 > 1 && n1 != n2) stop("'ext' must be of the same length as 'x'")
+  x = mapply(sub, r, ext, x, USE.NAMES = FALSE)
+  i = !(grepl(r, x) | ext == '')
+  x[i] = paste0(x[i], ext[i])
+  x
 }
 
 # counters for figures/tables
