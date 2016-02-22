b055d23b22948fc3e847aff71fe0c66d8aa536dd
Yihui Xie <xie@yihui.name>
2016-02-22 14:13:42 -0600

    complete the comment

--- inst/examples/build-all.R
+++ inst/examples/build-all.R
@@ -1,4 +1,4 @@
-# render
+# render the book to PDF and EPUB
 for (fmt in c('bookdown::pdf_book', 'bookdown::epub_book')) {
   res = bookdown:::Rscript(c(
     '-e', shQuote(sprintf("bookdown::render_book('index.Rmd', '%s')", fmt))
