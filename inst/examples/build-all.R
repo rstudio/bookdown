fa57f452a7b6e9383c771e7ec75e55a259f066f7
Yihui Xie <xie@yihui.name>
2016-02-21 22:57:06 -0600

    add a script to build the book to PDF, EPUB, and MOBI

---
+++ inst/examples/build-all.R
@@ -0,0 +1,22 @@
+# render
+for (fmt in c('bookdown::pdf_book', 'bookdown::epub_book')) {
+  res = bookdown:::Rscript(c(
+    '-e', shQuote(sprintf("bookdown::render_book('index.Rmd', '%s')", fmt))
+  ))
+  if (res != 0) stop('Failed to compile the book to ', fmt)
+}
+
+if (Sys.which('kindlegen') == '') {
+  if (.Platform$OS.type == 'windows' || Sys.info()[['sysname']] == 'Darwin')
+    stop('This script does not support Windows or Mac OS X')
+  bookdown:::in_dir(tempdir(), {
+    download.file('http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz', 'kindle.tar.gz', mode = 'wb')
+    system2('tar', c('zxf', 'kindle.tar.gz'))
+    dir.create('~/bin', showWarnings = FALSE)
+    file.rename('kindlegen', '~/bin/kindlegen')
+    if (Sys.which('kindlegen') == '')
+      Sys.setenv(PATH = paste0('~/bin:', Sys.getenv('PATH')))
+  })
+}
+
+bookdown::kindlegen()
