1ea4a4e2a2d1ba8c3530cad54c97a01c24b4c6a8
Yihui Xie <xie@yihui.name>
2016-03-18 21:52:23 -0500

    remove the hash when restoring scrolling position, otherwise once a reader clicks an internal link #foo, the browser will be stuck at that position, even the reader had scrolled away previously

--- inst/resources/gitbook/js/plugin-bookdown.js
+++ inst/resources/gitbook/js/plugin-bookdown.js
@@ -197,6 +197,7 @@ require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
   $(document).ready(function(e) {
     var pos = gs.get('bodyScrollTop');
     if (pos && pos.title === bookInner.find('.page-inner').find('h1,h2').first().text()) {
+      window.location.hash = '';
       if (pos.body !== 0) bookBody.scrollTop(pos.body);
       if (pos.inner !== 0) bookInner.scrollTop(pos.inner);
     }
