a3f2940b0e30d65361237711d96b34f89a90cc24
JJ Allaire <jj@rstudio.org>
2016-03-24 07:49:46 -0400

    port all logic from publish_book into site generators (eventually publish_book will get this info via the generators)

--- R/site.R
+++ R/site.R
@@ -4,15 +4,36 @@ bookdown_site <- function(input, ...) {
 
   on.exit(opts$restore(), add = TRUE)
 
+  # need to switch to the input directory for computing the config
+  oldwd = setwd(input)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  # load the config for the input directory
+  config = load_config()
+
+  # get the name from the config (default to the directory name
+  # if there is no name in the config)
+  name = with_ext(book_filename(config, fallback = FALSE), '')
+  if (is.null(name))
+    name = basename(normalizePath(input))
+
+  # get the book dir from the config
+  book_dir = output_dirname('_book', config, create = FALSE)
+  if (is.null(book_dir))
+    book_dir = '.'
+
   # Custom render function. bookdown rendering is more complex than
   # for most formats and as a result uses a custom R script (_render.R)
   # or Makefile to define what's required to render the book.
-  render <- function(output_format = NULL,
-                     quiet = FALSE,
-                     encoding = getOption("encoding"),
-                     ...) {
+  render = function(output_format, envir, quiet, encoding, ...) {
+
+    # switch to the input dir for the duration of render
+    oldwd <- setwd(input)
+    on.exit(setwd(oldwd), add = TRUE)
+
+    # perform the render
     if (length(script <- existing_r('_render', TRUE))) {
-      result = Rscript(script)
+      result = Rscript(c(script, ifelse(quiet, "--quiet", "")))
     } else if (file.exists('Makefile')) {
       result = system2('make')
     } else stop('Site rendering requires a _render.R or Makefile')
@@ -21,8 +42,8 @@ bookdown_site <- function(input, ...) {
 
   # return site generator
   list(
-    name = book_filename(),
-    output_dir = output_dirname('_book'),
+    name = name,
+    output_dir = book_dir,
     render = render
   )
 }
