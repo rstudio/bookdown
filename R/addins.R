6907616af74dfb7794fe6952a8722e5f73f1c611
Yihui Xie <xie@yihui.name>
2016-04-05 17:13:42 -0500

    update jQuery

--- R/addins.R
+++ R/addins.R
@@ -1,6 +1,6 @@
 jquery_dependency = function() {
   htmltools::htmlDependency(
-    'jquery', '2.2.0', bookdown_file('resources', 'jquery'),
+    'jquery', '2.2.3', bookdown_file('resources', 'jquery'),
     script = 'jquery.min.js'
   )
 }
