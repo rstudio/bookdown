729b2f4e2fc8506cb53523a9ac9a59cbd6369241
Yihui Xie <xie@yihui.name>
2016-04-08 13:50:24 -0500

    show latex source when the app is initialized, e.g. if user has selected a math expression in RStudio then launches the addin

--- inst/resources/mathquill/init.js
+++ inst/resources/mathquill/init.js
@@ -2,13 +2,15 @@ $(function() {
   var MQ = MathQuill.getInterface(2);
   var answer = MQ.MathField($('#latex_input')[0], {
     handlers: {
-      edit: function() {
-        var code = answer.latex();
-        $('textarea#latex_source').val(code).trigger('input');
-        Shiny.onInputChange('latex_source', code);
-      }
+      edit: update_latex
     }
   });
+  function update_latex() {
+    var code = answer.latex();
+    $('textarea#latex_source').val(code).trigger('input');
+    Shiny.onInputChange('latex_source', code);
+  }
+  update_latex();
   $('textarea#latex_source').on('input', function (e) {
     $(this).outerHeight(40).outerHeight(this.scrollHeight + 10);
   });
