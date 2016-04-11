38d4292b064ee7edbf85229411d8ef91ba3a0e84
Yihui Xie <xie@yihui.name>
2016-04-11 17:25:54 -0500

    use calibre() instead of kindlegen() to convert EPUB to MOBI (the latter does not work now for some reason)

--- inst/examples/_render.R
+++ inst/examples/_render.R
@@ -1,18 +1,6 @@
-if (Sys.which('kindlegen') == '') {
-  if (.Platform$OS.type == 'windows' || Sys.info()[['sysname']] == 'Darwin')
-    stop('This script does not support Windows or Mac OS X')
-  bookdown:::in_dir(tempdir(), {
-    download.file('http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz', 'kindle.tar.gz', mode = 'wb')
-    system2('tar', c('zxf', 'kindle.tar.gz'))
-    dir.create('~/bin', showWarnings = FALSE)
-    file.rename('kindlegen', '~/bin/kindlegen')
-    if (Sys.which('kindlegen') == '')
-      Sys.setenv(PATH = paste0('~/bin:', Sys.getenv('PATH')))
-  })
-}
-
 quiet = "--quiet" %in% commandArgs(FALSE)
 formats = commandArgs(TRUE)
+travis = !is.na(Sys.getenv('CI', NA))
 
 # provide default formats if necessary
 if (length(formats) == 0) formats = c(
@@ -23,11 +11,12 @@ for (fmt in formats) {
   cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = %s)", fmt, quiet)
   res = bookdown:::Rscript(c('-e', shQuote(cmd)))
   if (res != 0) stop('Failed to compile the book to ', fmt)
-  if (fmt == 'bookdown::epub_book') bookdown::kindlegen()
+  if (travis && fmt == 'bookdown::epub_book')
+    bookdown::calibre('_book/bookdown.epub', 'mobi')
 }
 
 # patch HTML files in gh-pages if built on Travis
-if (!is.na(Sys.getenv('CI', NA))) {
+if (travis) {
   r = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
   for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
     x = readLines(f)
