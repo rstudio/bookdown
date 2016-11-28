746d98ae7cbbd18d25fc64190f7f88c594b87932
Yihui Xie <xie@yihui.name>
2016-11-27 22:03:26 -0600

    move the mathquill addin to a separate script to make this package more lightweight (this addin is not essential, and RStudio IDE will remind users to install minUI anyway)

--- R/addins.R
+++ R/addins.R
@@ -5,40 +5,6 @@ jquery_dependency = function() {
   )
 }
 
-mathquill_dependency = function() {
-  htmltools::htmlDependency(
-    'mathquill', '0.10.1', bookdown_file('resources', 'mathquill'),
-    script = c('mathquill.min.js', 'init.js'), stylesheet = 'mathquill.css'
-  )
-}
-
-#' @importFrom miniUI miniPage gadgetTitleBar
-#' @importFrom htmltools attachDependencies tags
 mathquill = function() {
-  context = rstudioapi::getActiveDocumentContext()
-  shiny::runGadget(
-    miniPage(
-      gadgetTitleBar('Input LaTeX Math Expressions'),
-      attachDependencies(
-        tags$span(
-          id = 'latex_input',
-          context$selection[[1]]$text
-        ),
-        list(jquery_dependency(), mathquill_dependency())
-      ),
-      tags$p('LaTeX source:', style = 'margin: 1em auto 1em auto;'),
-      tags$textarea(
-        id = 'latex_source', readonly = 'readonly',
-        style = 'height: 40px; min-height: 40px;'
-      )
-    ),
-    server = function(input, output) {
-      shiny::observeEvent(input$done, {
-        code = input$latex_source
-        if (code != '') rstudioapi::insertText(code)
-        shiny::stopApp()
-      })
-    },
-    viewer = shiny::dialogViewer('Input LaTeX Math', height = 400)
-  )
+  sys.source(bookdown_file('scripts', 'mathquill.R'), new.env())
 }
