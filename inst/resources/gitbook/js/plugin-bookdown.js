957e36d5c1e2e86ab7e1e8b67f35321c1aed4c37
Yihui Xie <xie@yihui.name>
2017-03-10 00:39:07 -0600

    factor out the code to obtain the chapter title

--- inst/resources/gitbook/js/plugin-bookdown.js
+++ inst/resources/gitbook/js/plugin-bookdown.js
@@ -175,13 +175,16 @@ require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
   });
 
   var bookBody = $('.book-body'), bookInner = bookBody.find('.body-inner');
+  var chapterTitle = function() {
+    return bookInner.find('.page-inner').find('h1,h2').first().text();
+  };
   var saveScrollPos = function(e) {
     // save scroll position before page is reloaded
     gs.set('bodyScrollTop', {
       body: bookBody.scrollTop(),
       inner: bookInner.scrollTop(),
       focused: document.hasFocus(),
-      title: bookInner.find('.page-inner').find('h1,h2').first().text()
+      title: chapterTitle()
     });
   };
   $(document).on('servr:reload', saveScrollPos);
@@ -199,7 +202,7 @@ require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
   $(function(e) {
     var pos = gs.get('bodyScrollTop');
     if (pos) {
-      if (pos.title === bookInner.find('.page-inner').find('h1,h2').first().text()) {
+      if (pos.title === chapterTitle()) {
         if (pos.body !== 0) bookBody.scrollTop(pos.body);
         if (pos.inner !== 0) bookInner.scrollTop(pos.inner);
       }
