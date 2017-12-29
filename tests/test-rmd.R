2efad443403b283bd0d233d90187d388cda4c112
Yihui Xie <xie@yihui.name>
2017-12-29 11:03:09 -0600

    a test case for #504

--- tests/test-rmd.R
+++ tests/test-rmd.R
@@ -7,6 +7,9 @@ if (Sys.getenv('NOT_CRAN') == 'true') local({
   on.exit(unlink(setdiff(all_files(), files1), recursive = TRUE), add = TRUE)
 
   for (f in list.files('rmd', '[.]Rmd$', full.names = TRUE)) {
-    rmarkdown::render(f, envir = globalenv())
+    rmarkdown::render(f, envir = globalenv(), quiet = TRUE)
+  }
+  if (!any(readLines('rmd/equation-label.html') == 'y \\in \\mathbb{Z} \\tag{2}')) {
+    stop('Failed to render the equation label in equation-label.Rmd')
   }
 })
