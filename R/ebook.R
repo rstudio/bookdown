2ed8ff91cdf623620c79a3ab9fef7415625b24c8
Yihui Xie <xie@yihui.name>
2018-04-19 22:17:47 -0500

    do not resolve references in fenced code blocks

--- R/ebook.R
+++ R/ebook.R
@@ -83,9 +83,9 @@ process_markdown = function(input_file, from, pandoc_args, global, to_md = outpu
   x = read_utf8(intermediate_html)
   figs = parse_fig_labels(x, global)
   # resolve cross-references and update the Markdown input file
-  content = resolve_refs_md(
-    read_utf8(input_file), c(figs$ref_table, parse_section_labels(x)), to_md
-  )
+  content = read_utf8(input_file)
+  i = if (xfun::loadable('xaringan')) xaringan:::prose_index(content) else seq_along(content)
+  content[i] = resolve_refs_md(content[i], c(figs$ref_table, parse_section_labels(x)), to_md)
   if (to_md) content = gsub(
     '^\\\\BeginKnitrBlock\\{[^}]+\\}|\\\\EndKnitrBlock\\{[^}]+\\}$', '', content
   )
