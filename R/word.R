6239e3f1b08f0143ebc1e673baa67f5e3fa85359
atusy <30277794+atusy@users.noreply.github.com>
2019-07-25 07:19:29 +0900

    factor out repeated codes to office_document2_base

--- R/word.R
+++ R/word.R
@@ -1,24 +1,38 @@
+office_document2_base = function(base_format) {
+  force(base_format)
+
+  function(fig_caption = TRUE, md_extensions = NULL, pandoc_args = NULL, ...) {
+    from = rmarkdown::from_rmarkdown(fig_caption, md_extensions)
+
+    config = base_format(
+      fig_caption = fig_caption, md_extensions = md_extensions, pandoc_args = pandoc_args, ...
+    )
+    pre = config$pre_processor
+    config$pre_processor = function(metadata, input_file, ...) {
+      # Pandoc does not support numbered sections for Word, so figures/tables have
+      # to be numbered globally from 1 to n
+      process_markdown(input_file, from, pandoc_args, TRUE)
+      if (is.function(pre)) pre(metadata, input_file, ...)
+    }
+    post = config$post_processor
+    config$post_processor = function(metadata, input, output, clean, verbose) {
+      if (is.function(post)) output = post(metadata, input, output, clean, verbose)
+      move_output(output)
+    }
+    config$bookdown_output_format = 'docx'
+    config = set_opts_knit(config)
+    config
+  }
+}
+
 #' @rdname html_document2
 #' @export
-word_document2 = function(fig_caption = TRUE, md_extensions = NULL, pandoc_args = NULL, ...) {
-  from = rmarkdown::from_rmarkdown(fig_caption, md_extensions)
+word_document2 = office_document2_base(rmarkdown::word_document)
 
-  config = rmarkdown::word_document(
-    fig_caption = fig_caption, md_extensions = md_extensions, pandoc_args = pandoc_args, ...
-  )
-  pre = config$pre_processor
-  config$pre_processor = function(metadata, input_file, ...) {
-    # Pandoc does not support numbered sections for Word, so figures/tables have
-    # to be numbered globally from 1 to n
-    process_markdown(input_file, from, pandoc_args, TRUE)
-    if (is.function(pre)) pre(metadata, input_file, ...)
-  }
-  post = config$post_processor
-  config$post_processor = function(metadata, input, output, clean, verbose) {
-    if (is.function(post)) output = post(metadata, input, output, clean, verbose)
-    move_output(output)
-  }
-  config$bookdown_output_format = 'docx'
-  config = set_opts_knit(config)
-  config
-}
+#' @rdname html_document2
+#' @export
+odt_document2 = office_document2_base(rmarkdown::odt_document)
+
+#' @rdname html_document2
+#' @export
+powerpoint_presentation2 = office_document2_base(rmarkdown::powerpoint_presentation)
