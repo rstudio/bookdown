58b7bb51594f087e3e29d570a0dd28de2683ed00
atusy <30277794+atusy@users.noreply.github.com>
2019-07-29 21:14:56 +0900

    rename office_document2 to markdown_document2 and export it

--- R/word.R
+++ R/word.R
@@ -1,38 +1,45 @@
-office_document2_base = function(base_format) {
-  force(base_format)
+#' @rdname html_document2
+#' @export
+markdown_document2 = function(
+  fig_caption = TRUE, md_extensions = NULL, pandoc_args = NULL, ..., base_format
+) {
+  from = rmarkdown::from_rmarkdown(fig_caption, md_extensions)
 
-  function(fig_caption = TRUE, md_extensions = NULL, pandoc_args = NULL, ...) {
-    from = rmarkdown::from_rmarkdown(fig_caption, md_extensions)
-
-    config = base_format(
-      fig_caption = fig_caption, md_extensions = md_extensions, pandoc_args = pandoc_args, ...
-    )
-    pre = config$pre_processor
-    config$pre_processor = function(metadata, input_file, ...) {
-      # Pandoc does not support numbered sections for Word, so figures/tables have
-      # to be numbered globally from 1 to n
-      process_markdown(input_file, from, pandoc_args, TRUE)
-      if (is.function(pre)) pre(metadata, input_file, ...)
-    }
-    post = config$post_processor
-    config$post_processor = function(metadata, input, output, clean, verbose) {
-      if (is.function(post)) output = post(metadata, input, output, clean, verbose)
-      move_output(output)
-    }
-    config$bookdown_output_format = config$pandoc$to
-    config = set_opts_knit(config)
-    config
+  base_format = get_base_format(base_format)
+  config = base_format(
+    fig_caption = fig_caption, md_extensions = md_extensions, pandoc_args = pandoc_args, ...
+  )
+  pre = config$pre_processor
+  config$pre_processor = function(metadata, input_file, ...) {
+    # Pandoc does not support numbered sections for Word, so figures/tables have
+    # to be numbered globally from 1 to n
+    process_markdown(input_file, from, pandoc_args, TRUE)
+    if (is.function(pre)) pre(metadata, input_file, ...)
   }
+  post = config$post_processor
+  config$post_processor = function(metadata, input, output, clean, verbose) {
+    if (is.function(post)) output = post(metadata, input, output, clean, verbose)
+    move_output(output)
+  }
+  config$bookdown_output_format = config$pandoc$to
+  config = set_opts_knit(config)
+  config
 }
 
 #' @rdname html_document2
 #' @export
-word_document2 = office_document2_base(rmarkdown::word_document)
+word_document2 = function(...) {
+  markdown_document2(..., base_format = rmarkdown::word_document)
+}
 
 #' @rdname html_document2
 #' @export
-odt_document2 = office_document2_base(rmarkdown::odt_document)
+odt_document2 = function(...) {
+  markdown_document2(..., base_format = rmarkdown::odt_document)
+}
 
 #' @rdname html_document2
 #' @export
-powerpoint_presentation2 = office_document2_base(rmarkdown::powerpoint_presentation)
+powerpoint_presentation2 = function(...) {
+  markdown_document2(..., base_format = rmarkdown::powerpoint_presentation)
+}
