e7204613c5440a14e672a5eaa25b07366e9351d1
Romain LESUR <RLesur@users.noreply.github.com>
2019-09-20 17:34:23 +0200

    [gitbook] Copy to clipboard buttons for code blocks (#776)
    * use clipboard.js with gitbook
    
    * create a clipboard plugin
    
    * update changelog and add Romain Lesur to contributors

--- R/gitbook.R
+++ R/gitbook.R
@@ -91,11 +91,13 @@ gitbook_dependency = function(table_css) {
     'gitbook', '2.6.7', src = assets,
     stylesheet = file.path('css', c(
       'style.css', if (table_css) 'plugin-table.css', 'plugin-bookdown.css',
-      'plugin-highlight.css', 'plugin-search.css', 'plugin-fontsettings.css'
+      'plugin-highlight.css', 'plugin-search.css', 'plugin-fontsettings.css',
+      'plugin-clipboard.css'
     )),
     script = file.path('js', c(
-      app, 'lunr.js', 'plugin-search.js', 'plugin-sharing.js',
-      'plugin-fontsettings.js', 'plugin-bookdown.js', 'jquery.highlight.js'
+      app, 'lunr.js', 'clipboard.min.js', 'plugin-search.js', 'plugin-sharing.js',
+      'plugin-fontsettings.js', 'plugin-bookdown.js', 'jquery.highlight.js',
+      'plugin-clipboard.js'
     ))
   ))
 }
