88011ccd7da90c3865e66e227a4f0908f5a28ae5
christophe dervieux <christophe.dervieux@gmail.com>
2019-11-12 23:43:06 +0100

    add test to verify config is correctly handle when checking for google sharing

---
+++ tests/testit/test-gitbook.R
@@ -0,0 +1,30 @@
+library(testit)
+
+suppressWarnings(
+  assert(
+    'google sharing is disable',
+    # no sharing field
+    check_gb_config(list(search = TRUE)) %==%
+      list(search = TRUE),
+    # sharing is false => do nothing
+    check_gb_config(list(search = TRUE, sharing = FALSE)) %==%
+      list(search = TRUE, sharing = FALSE),
+    # sharing is null => do nothing
+    check_gb_config(list(search = TRUE, sharing = NULL)) %==%
+      list(search = TRUE, sharing = NULL),
+    # sharing has no google field => do nothing
+    check_gb_config(list(sharing = list(facebook = TRUE))) %==%
+      list(sharing = list(facebook = TRUE)),
+    # google sharing is activated => remove field
+    check_gb_config(list(sharing = list(facebook = TRUE, google = TRUE))) %==%
+      list(sharing = list(facebook = TRUE)),
+    # google sharing is deactivated => remove field
+    check_gb_config(list(sharing = list(facebook = TRUE, google = FALSE))) %==%
+      list(sharing = list(facebook = TRUE)),
+    check_gb_config(list(sharing = list(facebook = TRUE, google = NULL))) %==%
+      list(sharing = list(facebook = TRUE)),
+    # google is activated through the all field => remove it
+    check_gb_config(list(sharing = list(facebook = TRUE, all = c("google", "twitter", "linkedin")))) %==%
+      list(sharing = list(facebook = TRUE, all = c("twitter", "linkedin")))
+  )
+)
