26d3a7a9c0feb7a1c4170908d46f9c0a68b984c6
Yihui Xie <xie@yihui.name>
2019-12-05 11:17:17 -0600

    Pandoc 2.8 added another class 'hanging-indent' to the references div: <div id="refs" class="references hanging-indent">, so we shouldn't use the exact match to find this div

--- R/html.R
+++ R/html.R
@@ -902,7 +902,7 @@ restore_appendix_html = function(x, remove = TRUE) {
 
 # parse reference items so we can move them back to the chapter where they were used
 parse_references = function(x) {
-  i = which(x == '<div id="refs" class="references">')
+  i = grep('^<div id="refs" class="references[^"]*">$', x)
   if (length(i) != 1) return(list(refs = character(), html = x))
   r = '^<div id="(ref-[^"]+)">$'
   k = grep(r, x)
