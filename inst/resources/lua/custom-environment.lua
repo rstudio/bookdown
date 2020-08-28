f184c09694361aee4f0afe9d11eb55ec2ba29894
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-08-28 22:38:05 +0200

    refactor debuging helper

--- inst/resources/lua/custom-environment.lua
+++ inst/resources/lua/custom-environment.lua
@@ -12,31 +12,49 @@ for k in pairs(theorem_abbr) do
   table.insert(theorem_names, k)
 end
 
+-- just for debuging purpose
+local debug_mode = os.getenv("DEBUG_PANDOC_LUA") == "TRUE"
+
+function print_debug(obj, label, iter)
+    iter = iter or pairs
+    label = label or ""
+    label = "DEBUG: "..label
+    if (debug_mode) then
+        if (type(obj) == "string") then
+            print(label, obj)
+        else 
+            if type(obj) == "table" then
+                for k,v in iter(obj) do
+                    print(label, k, v)
+                end
+            end
+        end
+    end
+    return nil
+end
+
 -- Modify Pandoc AST for supported custom environment 
 Div = function (div)
     -- checking if classes correponds to a custom one
     local classes = div.classes
     -- we do nothing if no classes
     if (#classes == 0) then 
-        print("no classes")
+        print_debug("No classes in the Div")
         return div 
     end
-    -- test
-    for k,v in pairs(classes) do
-        print("classes", k, v)
-    end
+    
+    print_debug(classes, "Div classes ->")
 
     local theorem_type = {}
     for i,v in ipairs(classes) do
         if (theorem_abbr[v] ~= nil) then theorem_type[i] = v end
     end
     -- test
-    for k,v in pairs(theorem_type) do
-        print("types", k, v)
-    end
+    print_debug(theorem_type, "Found types ->")
+
     -- classes is not a supported one, we return as is
     if (#theorem_type == 0) then 
-        print("not supported")
+        print_debug("Type not supported")
         return div 
     end
     -- deal with special case of more than one corresponding classes
