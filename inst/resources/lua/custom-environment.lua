53e17008d2257a5f0149137d0c2954abdbb75964
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-09-03 18:36:44 +0200

    add rough support for eng_proof

--- inst/resources/lua/custom-environment.lua
+++ inst/resources/lua/custom-environment.lua
@@ -18,6 +18,13 @@ for k in pairs(theorem_abbr) do
   table.insert(theorem_names, k)
 end
 
+-- other special envs
+local proof_label = {
+    proof = 'Proof. ',
+    remark = 'Remark. ',
+    solution = 'Solution. '
+}
+
 -- just for debuging purpose
 local debug_mode = os.getenv("DEBUG_PANDOC_LUA") == "TRUE"
 
@@ -52,42 +59,38 @@ Div = function (div)
     print_debug(classes, "Div classes ->")
 
     local theorem_type = {}
+    local proof_type = {}
     for i,v in ipairs(classes) do
         if (theorem_abbr[v] ~= nil) then theorem_type[i] = v end
+        if (proof_label[v] ~= nil) then proof_type[i] = v end
     end
     -- test
     print_debug(theorem_type, "Found types ->")
 
     -- classes is not a supported one, we return as is
-    if (#theorem_type == 0) then
-        print_debug("Type not supported")
+    if (#theorem_type == 0 and #proof_label ~= 0) then
+        print_debug("Custom class not supported")
         return div
     end
     -- get the theorem type as string
-    if (#theorem_type ~= 1) then
+    if (#theorem_type ~= 1 or proof_type ~= 1) then
         -- warn if special case of more than one corresponding classes
         print("[WARNING] Only one custom environment can be used. Keeping the first.")
     end
     theorem_type = theorem_type[1]
-    print_debug(theorem_type, "type selected ->")
+    proof_type = proof_type[1]
+    print_debug(theorem_type, "theorem type selected ->")
+    print_debug(proof_type, "proof type selected ->")
 
     -- get the id if it exists - it will we use to build label for reference
     local id = div.identifier
     print_debug(id, "id found ->")
     -- remove unwanted identifier on the div, as it will be on the span
     div.identifier = ""
-
-    local label = ""
-    if (#id ~= 0) then
-        -- build label
-        label = string.format("%s:%s", theorem_abbr[theorem_type], id)
-        print_debug(label, "label for reference ->")
-    end
-
     -- get the attributes
     local options = div.attributes
 
-
+    -- return [name] for latex, and (name) for html
     function get_name(format, options)
         local name = options["data-name"]
         if (name == nil) then return "" end
@@ -104,47 +107,94 @@ Div = function (div)
         return name
     end
 
-    -- create the custom environment
-
-    -- TODO: should we support beamer also ?
-    if (FORMAT:match 'latex') then
-        
-        local label_part = ''
-        if (#label ~= 0) then
-            -- if no label referencing won't work but you can't reference without a label
-            -- so no one will try
-            label_part = string.format( "\n\\protect\\hypertarget{%s}{}\\label{%s}", label, label)
+    if (theorem_type ~= nil) then
+        print("Theorem part")
+        local label = ""
+        if (#id ~= 0) then
+            -- build label
+            label = string.format("%s:%s", theorem_abbr[theorem_type], id)
+            print_debug(label, "label for reference ->")
         end
 
-        local name = get_name('latex', options)
-        
-        table.insert(
-            div.content, 1,
-            pandoc.RawBlock('tex', string.format('\\begin{%s}%s%s', theorem_type, name, label_part))
-        )
-        table.insert(
-            div.content,
-            pandoc.RawBlock('tex', string.format('\\end{%s}', theorem_type))
-        )
+        -- create the custom environment
+
+        -- TODO: should we support beamer also ?
+        if (FORMAT:match 'latex') then
+            
+            local label_part = ''
+            if (#label ~= 0) then
+                -- if no label referencing won't work but you can't reference without a label
+                -- so no one will try
+                label_part = string.format( "\n\\protect\\hypertarget{%s}{}\\label{%s}", label, label)
+            end
+
+            local name = get_name('latex', options)
+            
+            table.insert(
+                div.content, 1,
+                pandoc.RawBlock('tex', string.format('\\begin{%s}%s%s', theorem_type, name, label_part))
+            )
+            table.insert(
+                div.content,
+                pandoc.RawBlock('tex', string.format('\\end{%s}', theorem_type))
+            )
+        end
+
+        if (FORMAT:match 'html') then
+            local name = get_name('html', options)
+
+            if (#label == 0) then
+                print("[WARNING] An id needs to be set in the custom divs for correct rendering")
+            else
+                table.insert(
+                    div.content, 1,
+                    pandoc.Para(
+                        pandoc.Span(
+                            pandoc.Strong(string.format("(#%s) %s", label, name)),
+                            {id = label, class = theorem_type}
+                        )
+                    )
+                )
+            end
+        end
     end
 
-    if (FORMAT:match 'html') then
-        local name = get_name('html', options)
+    if (proof_type ~= nil) then
+        -- create the custom environment
 
-        if (#label == 0) then
-            print("[WARNING] An id needs to be set in the custom divs for correct rendering")
-        else
+        -- TODO: should we support beamer also ?
+        if (FORMAT:match 'latex') then
+
+            local name = get_name('latex', options)
+            
+            table.insert(
+                div.content, 1,
+                pandoc.RawBlock('tex', string.format('\\begin{%s}%s', proof_type, name))
+            )
+            table.insert(
+                div.content,
+                pandoc.RawBlock('tex', string.format('\\end{%s}', proof_type))
+            )
+        end
+
+        if (FORMAT:match 'html') then
+            local name = get_name('html', options)
+
+            print_debug(name, "html name -> ")
+
+            -- if div is already processed by eng_proof, it passes also through this.
+            -- TODO: decide what to do to support both
             table.insert(
                 div.content, 1,
                 pandoc.Para(
-                    pandoc.Span(
-                        pandoc.Strong(string.format("(#%s) %s", label, name)),
-                        {id = label, class = theorem_type}
+                    pandoc.Span({
+                        pandoc.Emph(pandoc.Str(proof_label[proof_type])),
+                        pandoc.Str(name)},
+                        {class = proof_type}
                     )
                 )
             )
         end
     end
-
     return div
 end
