7a49610da9a4c50a12fae64e372dadf032866f44
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-09-23 11:59:55 +0200

    it does not work currently as with testthat and won't load the all package objects during tests.

--- tests/test-rmd.R
+++ tests/test-rmd.R
@@ -57,7 +57,7 @@ if (Sys.getenv('NOT_CRAN') == 'true') local({
   local({
     i18n <-bookdown:::load_config()$language$label$solution
     reg_span <- sprintf('<span .* class="solution"><em>%s</em>\\. </span>', i18n)
-    reg_env <- sprintf('<div class="(%s)">', paste(all_math_env, collapse = "|"))
+    reg_env <- sprintf('<div class="(%s)">', paste(bookdown:::all_math_env, collapse = "|"))
     if (!any(grepl(reg_env, readLines("rmd/custom-environments.html"))) ||
       !any(grepl(reg_span, readLines("rmd/custom-environments.html")))) {
     stop("Fail to apply the lua filter for custom environment")
