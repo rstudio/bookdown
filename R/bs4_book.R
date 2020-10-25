0b213dc4912726608d29ab535d4ae958536f0f62
Hadley Wickham <h.wickham@gmail.com>
2020-10-25 07:05:27 -0500

    Specify dependencies for R CMD check

--- R/bs4_book.R
+++ R/bs4_book.R
@@ -7,12 +7,6 @@
 #' @param config A list of configuration options for the gitbook style, such as
 #'   the font/theme settings.
 #' @export
-#' @examples
-#' withr::with_dir("inst/examples", render_book("index.Rmd", bs4_book(), quiet = TRUE, clean = FALSE))
-#' fs::dir_delete(file.path(tempdir(), "_book"))
-#' fs::dir_copy("inst/examples/_book", tempdir())
-#' browseURL(file.path(tempdir(), "_book/index.html"))
-#'
 bs4_book <- function(
                      theme = bs4_book_theme(),
                      ...,
@@ -20,6 +14,9 @@ bs4_book <- function(
                      pandoc_args = NULL,
                      extra_dependencies = NULL
                      ) {
+
+  check_packages(c("bootstraplib", "downlit", "jsonlite", "xml2"))
+
   config <- rmarkdown::html_document(
     toc = FALSE,
     number_sections = TRUE,
@@ -49,6 +46,18 @@ bs4_book <- function(
 
   # config <- common_format_config(config, "html")
   config
+}
+
+preview_book <- function(path = ".", ...) {
+  old <- setwd(path)
+  on.exit(setwd(old))
+
+  render_book("index.Rmd", bs4_book(...), quiet = TRUE, clean = FALSE)
+
+  unlink(file.path(tempdir(), "_book"))
+  file.copy("_book", tempdir(), recursive = TRUE)
+
+  browseURL(file.path(tempdir(), "_book/index.html"))
 }
 
 #' @export
@@ -316,4 +325,19 @@ strip_stop_words <- function(x) {
   # paste(tidytext::get_stopwords()$word, collapse = "|")
   pattern <- "\\b(i|me|my|myself|we|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|would|should|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|will)\\b ?"
   gsub(pattern, "", x, ignore.case = TRUE)
+}
+
+# helpers -----------------------------------------------------------------
+
+check_packages <- function(pkgs) {
+  inst <- vapply(pkgs, requireNamespace, quietly = TRUE, FUN.VALUE = logical(1))
+  if (all(inst)) {
+    return()
+  }
+
+  stop(
+    "Must install the following packages to use bs4_book():\n",
+    paste0("* ", pkgs[!inst], "\n"),
+    call. = FALSE
+  )
 }
