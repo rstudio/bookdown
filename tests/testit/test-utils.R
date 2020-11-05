c49e1946ece9e505184ba319d3aa332e6427b5fc
Yihui Xie <xie@yihui.name>
2020-11-05 14:20:44 -0600

    remove tests for clean_empty_dir() since they have been moved to xfun

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -52,21 +52,6 @@ assert('prepend_chapter_title() adds the chapter title to the page title', {
       '<title>chapter one | asdf qwer</title><meta property="og:title" content="chapter one | asdf qwer" />')
 })
 
-assert('correctly clean empty dir if required', {
-  # do nothing is NULL (#857)
-  (clean_empty_dir(NULL) %==% NULL)
-  # remove if empty
-  dir.create(temp_dir <- tempfile())
-  clean_empty_dir(temp_dir)
-  (dir_exists(temp_dir) %==% FALSE)
-  # do not remove if not empty
-  dir.create(temp_dir <- tempfile())
-  writeLines('test', tempfile(tmpdir = temp_dir))
-  (clean_empty_dir(temp_dir) %==% NULL)
-  (dir_exists(temp_dir) %==% TRUE)
-  unlink(temp_dir, recursive = TRUE)
-})
-
 assert('source_files() handles several configurations correcly', {
   get_files = function(files = NULL, dirs = NULL, ...) {
     source_files(config = list(rmd_files = files, rmd_subdir = dirs), ...)
