4a132a5b09be918fd08c24ecb40205c31dd35d1b
christophe dervieux <christophe.dervieux@gmail.com>
2020-12-16 09:28:42 +0100

    identify TRAVIS but not GHA
    in order for the piece of code regarding github page deployment to still work but not impact GHA deployment on bookdown.org

--- inst/examples/_render.R
+++ inst/examples/_render.R
@@ -1,6 +1,8 @@
 quiet = "--quiet" %in% commandArgs(FALSE)
 formats = commandArgs(TRUE)
-travis = !is.na(Sys.getenv('CI', NA))
+# travis is still use for github page deployment
+# TODO: Remove github page deployment
+travis = !is.na(Sys.getenv("CI", NA)) && !is.na(Sys.getenv('TRAVIS', NA))
 
 src = (function() {
   attr(body(sys.function()), 'srcfile')
@@ -22,6 +24,7 @@ for (fmt in formats) {
 }
 unlink('bookdown.log')
 
+# tweak the generated html files
 r = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
 for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
   x = readLines(f)
@@ -40,10 +43,10 @@ for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
   writeLines(x, f)
 }
 
-# When several format are rendered, usually when make all is called
+# When several format are rendered, usually when make all is called,
 # then we publish everything to bookdown.org
 if (length(formats) > 1) {
-  if (Sys.getenv("CI", FALSE)) {
+  if (!is.na(Sys.getenv("CI", NA))) {
     # On CI connect to server, using API KEY and deploy using appId
     rsconnect::addConnectServer('https://bookdown.org', 'bookdown.org')
     rsconnect::connectApiUser(
