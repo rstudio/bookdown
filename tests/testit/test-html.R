94c5ebc69b9a812dacd10924285552ad5bfe2df5
christophe dervieux <christophe.dervieux@gmail.com>
2021-03-15 11:44:02 +0100

    Add test

---
+++ tests/testit/test-html.R
@@ -0,0 +1,14 @@
+library(testit)
+
+assert("parse reference correctly", {
+  res <- parse_fig_labels("<caption>(#tab:foo) caption </caption>")
+  (names(res$ref_table) %==% "tab:foo")
+  (grepl("Table 1", res$content, fixed = TRUE))
+  (!grepl("(#tab:foo)", res$content, fixed = TRUE))
+  # works with indented too
+  # https://github.com/rstudio/gt/issues/719
+  res <- parse_fig_labels("  <caption>(#tab:foo) caption </caption>")
+  (names(res$ref_table) %==% "tab:foo")
+  (grepl("Table 1", res$content, fixed = TRUE))
+  (!grepl("(#tab:foo)", res$content, fixed = TRUE))
+})
