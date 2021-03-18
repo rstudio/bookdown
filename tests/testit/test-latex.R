8b30ea4af09f8bcaac7d4c98433411b13e1cd428
christophe dervieux <christophe.dervieux@gmail.com>
2021-03-18 16:39:42 +0100

    Add some tests

---
+++ tests/testit/test-latex.R
@@ -0,0 +1,13 @@
+library(testit)
+
+assert("insert TeX syntax for bib in toc correctly", {
+  (add_toc_bib("\\anything else unchanged") %==% "\\anything else unchanged")
+  (add_toc_bib("\\bibliography{bib1.bib}") %==%
+    "\\bibliography{bib1.bib}\n\\addcontentsline{toc}{section}{\\bibname}")
+  (add_toc_bib("\\bibliography{bib1.bib,bib2.bib}") %==%
+    "\\bibliography{bib1.bib,bib2.bib}\n\\addcontentsline{toc}{section}{\\bibname}")
+  (add_toc_bib("\\printbibliography") %==%
+    "\\printbibliography\n\\addcontentsline{toc}{section}{\\bibname}")
+  (add_toc_bib("\\printbibliography[title=References]") %==%
+    "\\printbibliography[title=References]\n\\addcontentsline{toc}{section}{\\bibname}")
+})
