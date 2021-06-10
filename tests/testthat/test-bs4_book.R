ab1ec43f925195224b180bdf4432cc0fe91d7430
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-06-10 13:17:29 +0200

    more testing

--- tests/testthat/test-bs4_book.R
+++ tests/testthat/test-bs4_book.R
@@ -1,4 +1,4 @@
-test_that("bs4_book() repo specification works", {
+test_that("bs4_book() repo specification works - default case", {
   temp_path <- withr::local_tempdir()
   book <- create_minimal_bs_book(
     path = temp_path,
@@ -21,7 +21,9 @@ test_that("bs4_book() repo specification works", {
     "https://github.com/hadley/ggplot2-book/blob/master/index.Rmd"
   )
 
+})
 
+test_that("bs4_book() repo specification works - branch and subdir", {
   temp_path <- withr::local_tempdir()
   book <- create_minimal_bs_book(
     path = temp_path,
@@ -35,8 +37,15 @@ test_that("bs4_book() repo specification works", {
   )
   html <- xml2::read_html(file.path(temp_path, "_book", "index.html"))
 
+  repo_a <- xml2::xml_find_first(html, "//a[@id='book-repo']")
+
   expect_equal(
-    xml2::xml_attr(xml2::xml_find_first(html, "//a[@id='book-repo']"), "href"),
+    xml2::xml_attr(xml2::xml_child(repo_a), "class"),
+    "fab fa-github"
+  )
+
+  expect_equal(
+    xml2::xml_attr(repo_a, "href"),
     "https://github.com/hadley/ggplot2-book"
   )
   expect_equal(
@@ -46,5 +55,22 @@ test_that("bs4_book() repo specification works", {
   expect_equal(
     xml2::xml_attr(xml2::xml_find_first(html, "//a[@id='book-source']"), "href"),
     "https://github.com/hadley/ggplot2-book/blob/main/book/index.Rmd"
+  )
+})
+
+
+test_that("bs4_book() repo specification works - GitLab", {
+  temp_path <- withr::local_tempdir()
+  book <- create_minimal_bs_book(
+    path = temp_path,
+    output_options = list(
+      repo = "https://gitlab.com/hadley/ggplot2-book"
+    )
+  )
+  html <- xml2::read_html(file.path(temp_path, "_book", "index.html"))
+
+  expect_equal(
+    xml2::xml_attr(xml2::xml_child(xml2::xml_find_first(html, "//a[@id='book-repo']")), "class"),
+    "fab fa-gitlab"
   )
 })
