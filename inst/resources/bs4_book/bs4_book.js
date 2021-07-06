a3269dc0653f6a8ae6c6e144f647527d98af9f4e
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-06 16:11:05 +0200

    only apply button to source block

--- inst/resources/bs4_book/bs4_book.js
+++ inst/resources/bs4_book/bs4_book.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
   if(ClipboardJS.isSupported()) {
     // Insert copy buttons
     var copyButton = "<div class='copy'><button type='button' class='btn btn-copy' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='popover' data-placement='top' data-trigger='hover'><i class='bi'></i></button></div>";
-    $(copyButton).appendTo("pre");
+    $(copyButton).appendTo("pre.sourceCode");
     // Initialize tooltips:
     $('.btn-copy').tooltip({container: 'body', boundary: 'window'});
 
