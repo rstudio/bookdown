2d35472e2d18c800b2f89e81c82ad55379179e9c
christophe dervieux <christophe.dervieux@gmail.com>
2021-07-12 17:23:38 +0200

    Keep the scroll and fix the button on the div

--- inst/resources/bs4_book/bs4_book.js
+++ inst/resources/bs4_book/bs4_book.js
@@ -107,15 +107,15 @@ function changeTooltipMessage(element, msg) {
 $(document).ready(function() {
   if(ClipboardJS.isSupported()) {
     // Insert copy buttons
-    var copyButton = "<div class='copy'><button type='button' class='btn btn-copy' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='popover' data-placement='top' data-trigger='hover'><i class='bi'></i></button></div>";
-    $(copyButton).appendTo("pre.sourceCode");
+    var copyButton = "<button type='button' class='btn btn-copy' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='popover' data-placement='top' data-trigger='hover'><i class='bi'></i></button>";
+    $(copyButton).appendTo("div.sourceCode");
     // Initialize tooltips:
     $('.btn-copy').tooltip({container: 'body', boundary: 'window'});
 
     // Initialize clipboard:
     var clipboard = new ClipboardJS('.btn-copy', {
       text: function(trigger) {
-        return trigger.parentNode.previousSibling.textContent;
+        return trigger.parentNode.textContent;
       }
     });
 
