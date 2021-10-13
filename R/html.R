dd00ae08fc71ef36899df1235c0a91ae6e97aed7
Christophe Dervieux <cderv@rstudio.com>
2021-10-13 15:29:00 +0200

    Fix issue with recent pandoc and html_book() regarding toc.css (#1270)

--- R/html.R
+++ R/html.R
@@ -222,8 +222,7 @@ tufte_html2 = function(..., number_sections = FALSE) {
 build_chapter = function(
   head, toc, chapter, link_prev, link_next, rmd_cur, html_cur, foot
 ) {
-  # add a has-sub class to the <li> items that has sub lists
-  toc = gsub('^(<li>)(.+<ul>)$', '<li class="has-sub">\\2', toc)
+  toc = add_toc_class(toc)
   paste(c(
     head,
     '<div class="row">',
@@ -245,6 +244,10 @@ build_chapter = function(
     '</div>',
     foot
   ), collapse = '\n')
+}
+
+add_toc_class = function(toc) {
+  gsub('^(<li>)(.+)(?<!<\\/li>)$', '<li class="has-sub">\\2', toc, perl = TRUE)
 }
 
 r_chap_pattern = '^<!--chapter:end:(.+)-->$'
