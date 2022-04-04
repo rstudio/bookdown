ecc9bae0156bb9666b7a9952e90ae9d6c0b01524
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-04-04 15:35:23 +0200

    same order of argument

--- R/render.R
+++ R/render.R
@@ -166,7 +166,7 @@ render_book = function(
   )
 
   res = if (new_session) {
-    render_new_session(files, main, config, output_format, output_options, output_options = output_options, clean, envir, ...)
+    render_new_session(files, main, config, output_format, output_options, clean, envir, output_options = output_options, ...)
   } else {
     render_cur_session(files, main, config, output_format, clean, envir, output_options = output_options, ...)
   }
