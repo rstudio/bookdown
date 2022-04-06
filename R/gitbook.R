b13bdf1235672b17ae6a95fa46a5569ea6173aad
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-04-06 16:24:33 +0200

    Remove empty spans due to anchor sections
    Fixes https://github.com/rstudio/bookdown/issues/1326

--- R/gitbook.R
+++ R/gitbook.R
@@ -192,6 +192,9 @@ gitbook_toc = function(x, cur, config) {
   if (i2 - i1 < 2) return(x)
   toc = x[(i1 + 1):(i2 - 1)]
 
+  # Remove possible empty span due to anchor section
+  toc = gsub("<span></span>(?=</a>)", "", toc, perl = TRUE)
+
   # numbered sections
   r = '^<li><a href="([^#]*)(#[^"]+)"[^>]*><span class="toc-section-number">([.A-Z0-9]+)</span>(.+)(</a>.*)$'
   i = grep(r, toc)
