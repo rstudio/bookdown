bf41396a518c43773f5309af35fd08353619ce24
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-04-06 15:59:05 +0200

    support pandoc 2.18 change in gitbook_toc()

--- R/gitbook.R
+++ R/gitbook.R
@@ -193,7 +193,7 @@ gitbook_toc = function(x, cur, config) {
   toc = x[(i1 + 1):(i2 - 1)]
 
   # numbered sections
-  r = '^<li><a href="([^#]*)(#[^"]+)"><span class="toc-section-number">([.A-Z0-9]+)</span>(.+)(</a>.*)$'
+  r = '^<li><a href="([^#]*)(#[^"]+)"[^>]*><span class="toc-section-number">([.A-Z0-9]+)</span>(.+)(</a>.*)$'
   i = grep(r, toc)
   toc[i] = gsub(
     r,
@@ -203,7 +203,7 @@ gitbook_toc = function(x, cur, config) {
   toc[i] = sub(' data-path="">', paste0(' data-path="', with_ext(cur, '.html'), '">'), toc[i])
 
   # unnumbered sections
-  r = '^<li><a href="([^#]*)(#[^"]+)">((.+)</a>.*)$'
+  r = '^<li><a href="([^#]*)(#[^"]+)"[^>]*>((.+)</a>.*)$'
   i = grep(r, toc)
   toc[i] = gsub(
     r,
