3898705684ab831bc189e8d9c99e897f338e0454
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-09-25 18:21:07 +0200

    Allow space between name and = for attributes src / href when searching for resources
    Follow feedback at https://community.rstudio.com/t/bookdown-image-with-a-weblink/172542/5

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # CHANGES IN bookdown VERSION 0.36
 
+- Fix an issue with parsing resources from raw HTML code (thanks, @lennylin, https://community.rstudio.com/t/bookdown-image-with-a-weblink/172542)
+
 
 # CHANGES IN bookdown VERSION 0.35
 
--- R/html.R
+++ R/html.R
@@ -1083,7 +1083,7 @@ move_files_html = function(output, lib_dir) {
   if (is.null(o <- opts$get('output_dir'))) return()
   x = read_utf8(output)
   # detect local resources used in HTML
-  r = '[- ](src|href)="([^"]+)"'
+  r = '[- ](src|href)\\s*=\\s*"([^"]+)"'
   m = gregexpr(r, x)
   f = unlist(lapply(regmatches(x, m), function(z) {
     if (length(z) == 0) z else gsub(r, '\\2', z)
