998623d7fc65ce0c3725ab320fdfc2c8e0e86805
Yihui Xie <xie@yihui.name>
2024-12-06 10:30:51 -0600

    cosmetic [ci skip]

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
 # CHANGES IN bookdown VERSION 0.42
 
 - New option in `gitbook`'s font settings menu to control line spacing (thanks, @hayden-MB, #1479).
-- New configuration setting `include_md` to control whether search includes `.md` source files in addition to `.Rmd`.
+
+- New configuration setting `include_md` to control whether the input search includes `.md` source files in addition to `.Rmd` (thanks, @katrinabrock #1483, @kylelundstedt #956).
 
 # CHANGES IN bookdown VERSION 0.41
 
--- R/utils.R
+++ R/utils.R
@@ -86,7 +86,7 @@ book_filename = function(config = load_config(), fallback = TRUE) {
 
 source_files = function(format = NULL, config = load_config(), all = FALSE) {
   subdir = config[['rmd_subdir']]; subdir_yes = isTRUE(subdir) || is.character(subdir)
-  ext_regex <- if(isTRUE(config[['include_md']])) '[.]R?md$' else '[.]Rmd$'
+  ext_regex = if (isTRUE(config[['include_md']])) '[.]R?md$' else '[.]Rmd$'
   # a list of Rmd chapters
   files = list.files('.', ext_regex, ignore.case = TRUE)
   # content in subdir if asked
