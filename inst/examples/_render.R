98b9f55c16dbc9ce02ef83edce5cb0331f59e4fa
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-12-01 19:02:10 +0100

    Full book is now deployed to gh-pages to be deployed to Connect cloud (#1513)

--- inst/examples/_render.R
+++ inst/examples/_render.R
@@ -1,9 +1,5 @@
 quiet = "--quiet" %in% commandArgs(FALSE)
 formats = commandArgs(TRUE)
-# travis is still use for github page deployment
-# TODO: Remove github page deployment
-ghpages = identical(Sys.getenv("DEPLOY_GH_PAGES", NA), "true")
-
 
 src = (function() {
   attr(body(sys.function()), 'srcfile')
@@ -34,44 +30,11 @@ for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
   x = readLines(f)
 
   if (length(i <- grep('^\\s*<body>\\s*$', x)) == 0) next
-  # patch HTML files when published to gh-pages to redirect to bookdown.org
-  if (ghpages) x[i[1]] = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
   i = grep('<i class="fa fa-circle-o-notch fa-spin"></i><a href="./">.+</a>', x)[1]
   # shorter title on the toolbar
   if (!is.na(i)) x[i] = gsub('bookdown: ', '', x[i], fixed = TRUE)
-  # Remove some lines when published to gh-pages
-  if (ghpages) {
-    i = c(
-      grep('&lt;bytecode: 0x[0-9a-f]+&gt;$', x),
-      grep('^\\s*<meta name="generator" content="bookdown [.0-9]+ and GitBook [.0-9]+" />$', x),
-      grep('^<meta name="date" content="[-0-9]+" />$', x)
-    )
-    if (length(i)) x = x[-i]
-  }
 
   writeLines(x, f)
-}
-
-# When several format are rendered, usually when make all is called,
-# then we publish everything to bookdown.org
-if (length(formats) > 1 && !ghpages) {
-  message(">> Publishing Books")
-  if (!is.na(Sys.getenv("CI", NA))) {
-    # On CI connect to server, using API KEY and deploy using appId
-    rsconnect::addServer('https://bookdown.org', 'bookdown.org')
-    rsconnect::connectApiUser(
-      account = 'GHA', server = 'bookdown.org',
-      apiKey = Sys.getenv('CONNECT_API_KEY')
-    )
-    rsconnect::deploySite(
-      appId = Sys.getenv('CONTENT_ID'),
-      server = 'bookdown.org',
-      render = 'none', logLevel = 'verbose',
-      forceUpdate = TRUE)
-  } else {
-    # for local deployment when rsconnect/ is available
-    bookdown::publish_book('bookdown', server = 'bookdown.org', render = 'none')
-  }
 }
 
 setwd(owd)
