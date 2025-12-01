937e5214c80190c46c68101e9975dcd1b2323634
Neal Richardson <neal.p.richardson@gmail.com>
2025-12-01 15:09:43 -0500

    Make connect.posit.cloud the default if no accounts configured

--- R/publish.R
+++ R/publish.R
@@ -12,33 +12,27 @@
 #' @param account Account name to publish to. Will default to any previously
 #'   published to account or any single account already associated with
 #'   \code{server}.
-#' @param server Server to publish to (by default beta.rstudioconnect.com but
-#'   any RStudio Connect server can be published to).
+#' @param server Server to publish to (by default connect.posit.cloud but
+#'   any Posit Connect server can be published to).
 #'
 #' @export
 publish_book = function(
   name = NULL, account = NULL, server = NULL, render = c("none", "local", "server")
 ) {
 
-  # if there are no RS Connect accounts setup on this machine
-  # then offer to add one for bookdown.org
+  # if there are no Connect accounts setup on this machine
+  # then offer to add one for connect.posit.cloud
   accounts <- rsconnect::accounts()
   accounts <- subset(accounts, server != "shinyapps.io")
   if (is.null(accounts) || nrow(accounts) == 0) {
-
-    # add the server if we need to
-    servers = rsconnect::servers()
-    if (nrow(subset(servers, name == 'bookdown.org')) == 0)
-      rsconnect::addServer("https://bookdown.org/__api__", 'bookdown.org')
-
     # see if they want to configure an account (bail if they don't)
-    message('You do not currently have a bookdown.org publishing account ',
+    message('You do not currently have a publishing account ',
             'configured on this system.')
     result = readline('Would you like to configure one now? [Y/n]: ')
     if (tolower(result) == 'n') return(invisible())
 
     # configure the account
-    rsconnect::connectUser(server = 'bookdown.org')
+    rsconnect::connectCloudUser()
   }
 
   # deploy the book
